5005adeb259fb4b689f580a17efd24f1ef271f30
fischermatte <fischermatte@posteo.net>
2020-02-10 21:58:45 +0100

    added navigation

--- projects/geolud-client/src/app/core/core.module.ts
+++ projects/geolud-client/src/app/core/core.module.ts
@@ -3,10 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FlexModule } from '@angular/flex-layout';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
-  exports: [HeaderComponent, FooterComponent, FlexModule],
-  imports: [CommonModule, FlexModule],
+  declarations: [HeaderComponent, FooterComponent, NavigationComponent],
+  exports: [HeaderComponent, FooterComponent, FlexModule, NavigationComponent],
+  imports: [CommonModule, FlexModule, LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule],
 })
 export class CoreModule {}

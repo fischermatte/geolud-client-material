aace2fc2498db0dbecd2293083850e2483e85614
fischermatte <fischermatte@posteo.net>
2020-02-10 21:53:08 +0100

    added fxflex

--- projects/geolud-client/src/app/core/core.module.ts
+++ projects/geolud-client/src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { FlexModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  exports: [HeaderComponent, FooterComponent],
-  imports: [CommonModule],
+  exports: [HeaderComponent, FooterComponent, FlexModule],
+  imports: [CommonModule, FlexModule],
 })
 export class CoreModule {}
